60b8a544d7e2ccd50fd655592e9651e674234960
DonTouzi <dontouzi@gmail.com>
2021-07-25 00:45:22 +0200

    Use state completed

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [person, setPerson] = useState({
+    name: "Walid",
+    age: 28,
+    message: "Lorem ipsum dolor sit amet",
+  });
+
+  const changeMessage = () => {
+    setPerson({ ...person, message: "Hi guys, how are you!" });
+  };
+  return (
+    <>
+      <h4>{person.name}</h4>
+      <h4>{person.age}</h4>
+      <h4>{person.message}</h4>
+      <button type="button" className="btn" onClick={changeMessage}>
+        change message
+      </button>
+    </>
+  );
 };
 
 export default UseStateObject;
--- src/tutorial/1-useState/setup/5-useState-counter.js
+++ src/tutorial/1-useState/setup/5-useState-counter.js
@@ -1,7 +1,46 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateCounter = () => {
-  return <h2>useState counter example</h2>;
+  const [counter, setCounter] = useState(0);
+
+  const reset = () => {
+    setCounter(0);
+  };
+
+  const increment = () => {
+    setCounter(counter + 1);
+  };
+
+  const decrement = () => {
+    setCounter(counter - 1);
+  };
+
+  const complexIncrement = () => {
+    setTimeout(() => {
+      setCounter((prevState) => prevState + 1);
+    }, 2000);
+  };
+
+  return (
+    <>
+      <h1 style={{ margin: "20px auto" }}>simple counter</h1>
+      <h2>{counter}</h2>
+      <button type="button" className="btn" onClick={decrement}>
+        -
+      </button>
+      <button type="button" className="btn" onClick={reset}>
+        reset
+      </button>
+      <button type="button" className="btn" onClick={increment}>
+        +
+      </button>
+      <h1 style={{ margin: "20px auto" }}>complex counter</h1>
+      <h2>{counter}</h2>
+      <button type="button" className="btn" onClick={complexIncrement}>
+        increase with delay
+      </button>
+    </>
+  );
 };
 
 export default UseStateCounter;
