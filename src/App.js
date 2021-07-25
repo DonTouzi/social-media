55838d647dd3fc08be95fcb081ea95341b9bc7e4
DonTouzi <dontouzi@gmail.com>
2021-07-25 15:22:16 +0200

    Add refs

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
+import Setup from "./tutorial/5-useRef/setup/1-useRef-basics";
 
 function App() {
   return (
     <div className="container">
-      <h2>Advanced Tutorial</h2>
+      <Setup />
     </div>
   );
 }
