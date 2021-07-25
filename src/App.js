cba834fbe28b9039687678aff0405100816208b3
DonTouzi <dontouzi@gmail.com>
2021-07-25 21:45:46 +0200

    Add custom hooks

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
+import Setup from "./tutorial/9-custom-hooks/setup/1-fetch-example";
 
 function App() {
   return (
     <div className="container">
-      <h2>Advanced Tutorial</h2>
+      <Setup />
     </div>
   );
 }
