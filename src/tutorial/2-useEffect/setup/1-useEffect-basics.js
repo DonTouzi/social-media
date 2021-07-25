90d3fc66d901db4c32939f3aec7e9f7babb1e05b
DonTouzi <dontouzi@gmail.com>
2021-07-25 02:27:26 +0200

    Github users interface

--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -1,9 +1,22 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 // by default runs after every re-render
 // cleanup function
 // second parameter
 const UseEffectBasics = () => {
-  return <h2>useEffect Basics</h2>;
+  const [value, setValue] = useState(0);
+
+  useEffect(() => {
+    if (value > 0) document.title = `New messages(${value})`;
+  }, [value]);
+
+  return (
+    <>
+      <h2>{value}</h2>
+      <button type="button" className="btn" onClick={() => setValue(value + 1)}>
+        Increase
+      </button>
+    </>
+  );
 };
 
 export default UseEffectBasics;
--- src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
+++ src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
@@ -1,9 +1,36 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
-const url = 'https://api.github.com/users';
+const url = "https://api.github.com/users";
 
 const UseEffectFetchData = () => {
-  return <h2>fetch data</h2>;
+  const [users, setUsers] = useState([]);
+
+  const getUsers = async () => {
+    const response = await fetch(url);
+    const users = await response.json();
+    setUsers(users);
+  };
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  return (
+    <React.Fragment>
+      <h2>github users</h2>
+      <ul className="users">
+        {users.map((user) => (
+          <li key={user.id}>
+            <img src={user.avatar_url} alt={user.login} />
+            <div>
+              <h4>{user.login}</h4>
+              <a href={user.html_url}>profile</a>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </React.Fragment>
+  );
 };
 
 export default UseEffectFetchData;
