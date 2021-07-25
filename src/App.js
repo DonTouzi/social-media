8741d88a4f6cd8e3c53e053030a32f78b4c5bb5e
DonTouzi <dontouzi@gmail.com>
2021-07-26 00:23:54 +0200

    Add callbacks

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
+import Setup from "./tutorial/12-memo-useMemo-useCallback/setup";
 
 function App() {
   return (
     <div className="container">
-      <h2>Advanced Tutorial</h2>
+      <Setup />
     </div>
   );
 }
--- src/tutorial/12-memo-useMemo-useCallback/setup/index.js
+++ src/tutorial/12-memo-useMemo-useCallback/setup/index.js
@@ -1,48 +1,63 @@
-import React, { useState, useEffect, useCallback, useMemo } from 'react'
-import { useFetch } from '../../9-custom-hooks/final/2-useFetch'
+import React, { useState, useEffect, useCallback, useMemo } from "react";
+import { useFetch } from "../../9-custom-hooks/final/2-useFetch";
 
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/javascript-store-products'
+const url = "https://course-api.com/javascript-store-products";
 
 // every time props or state changes, component re-renders
 
 const Index = () => {
-  const { products } = useFetch(url)
-  const [count, setCount] = useState(0)
+  const { products } = useFetch(url);
+  const [count, setCount] = useState(0);
+  const [cart, setCart] = useState(0);
+
+  const addToChart = useCallback(() => {
+    setCart(cart + 1);
+  }, [cart]);
 
   return (
     <>
       <h1>Count : {count}</h1>
-      <button className='btn' onClick={() => setCount(count + 1)}>
+      <button className="btn" onClick={() => setCount(count + 1)}>
         click me
       </button>
-      <BigList products={products} />
+      <h1 style={{ marginTop: "3rem" }}>cart: {cart}</h1>
+      <BigList products={products} addToChart={addToChart} />
     </>
-  )
-}
+  );
+};
 
-const BigList = ({ products }) => {
+const BigList = React.memo(({ products, addToChart }) => {
   return (
-    <section className='products'>
+    <section className="products">
       {products.map((product) => {
-        return <SingleProduct key={product.id} {...product}></SingleProduct>
+        return (
+          <SingleProduct
+            key={product.id}
+            {...product}
+            addToChart={addToChart}
+          ></SingleProduct>
+        );
       })}
     </section>
-  )
-}
+  );
+});
 
-const SingleProduct = ({ fields }) => {
-  let { name, price } = fields
-  price = price / 100
-  const image = fields.image[0].url
+const SingleProduct = ({ fields, addToChart }) => {
+  let { name, price } = fields;
+  price = price / 100;
+  const image = fields.image[0].url;
 
   return (
-    <article className='product'>
+    <article className="product">
       <img src={image} alt={name} />
       <h4>{name}</h4>
       <p>${price}</p>
+      <button type="button" className="btn" onClick={addToChart}>
+        add to cart
+      </button>
     </article>
-  )
-}
-export default Index
+  );
+};
+export default Index;
